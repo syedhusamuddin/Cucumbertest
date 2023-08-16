8e8132ae9f654bc02a4fa143357668944242d368
shusamuddin <shusamuddin@USHYDSHUSA12.us.deloitte.com>
2023-08-16 17:10:29 +0530

    first commit

---
+++ src/test/java/stepdefination/login.java
@@ -0,0 +1,26 @@
+package stepdefination;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class login {
+	
+	@Given("I want to write a step with precondition")
+	public void i_want_to_write_a_step_with_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("some other precondition")
+	public void some_other_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I complete action")
+	public void i_complete_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
---
+++ src/test/java/testrunner/testRunner.java
@@ -0,0 +1,17 @@
+package testrunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		tags="@test",
+		features="src/test/resources/feature", 
+		glue= {"stepdefination"},
+		plugin = {"json:target/cucumber.json"})
+
+public class testRunner {
+
+}
