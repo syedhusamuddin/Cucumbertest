9bdc926095f1351817c3903b56d991fb44e5af02
syedhusamuddin <34361206+syedhusamuddin@users.noreply.github.com>
2023-08-17 10:02:29 +0530

    Update login.java

--- src/test/java/stepdefination/login.java
+++ src/test/java/stepdefination/login.java
@@ -8,6 +8,7 @@ public class login {
 	@Given("I want to write a step with precondition")
 	public void i_want_to_write_a_step_with_precondition() {
 	    System.out.println("I want to write a step with precondition");
+	System.out.println("Code pushed by dev 2");
 	}
 
 	@Given("some other precondition")
